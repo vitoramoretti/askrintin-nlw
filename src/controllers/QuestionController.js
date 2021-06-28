8ab1bb9448a58303529ba94408c77d31e79fe978
Vitor Amoretti <vitoramoretti@AmorUnas-Mac-mini.local>
2021-06-28 20:59:16 -0300

    last adjustments on system during class 5

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,8 +1,40 @@
+const Database = require('../db/config')
+
 module.exports = {
-    index(req, res) {
+    async index(req, res) {
+        const db = await Database()
         const roomId = req.params.room
-        const questionId = req.param.question
+        const questionId = req.params.question
         const action = req.params.action
         const password = req.body.password
+
+        const checkRoom = await db.get(`SELECT * FROM rooms WHERE id = ${roomId}`)
+        if(checkRoom.pass === password) {
+            if(action == "delete"){
+                await db.run(`DELETE FROM questions WHERE id = ${questionId}`)
+            }else if(action == "check"){
+                await db.run(`UPDATE questions SET read = 1 WHERE id = ${questionId}`)
+            }
+            res.redirect(`/room/${roomId}`)
+        }else {
+            res.render('incorrect-pass', {roomId: roomId})
+        }
+
+    },
+    async create(req, res) {
+        const db = await Database()
+        const roomId = req.params.room
+        const question = req.body.question
+
+        await db.run(`INSERT INTO questions (
+            title,
+            room,
+            read
+        )VALUES(
+            "${question}",
+            ${roomId},
+            0
+        )`)
+        res.redirect(`/room/${roomId}`)
     }
 }
